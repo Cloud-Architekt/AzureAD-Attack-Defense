bdb4c51c57e2d348962e0f53d09bec5936c76e1f
Thomas Naunheim <47817884+Cloud-Architekt@users.noreply.github.com>
2023-06-04 10:06:16 +0000

    Updated permission script to include Playbook

--- config/deploy/AADSCA-AddedPermToLogicAppMSI.ps1
+++ config/deploy/AADSCA-AddedPermToLogicAppMSI.ps1
@@ -1,23 +1,22 @@
 Connect-AzureAD
 
 $miObjectID = $null
-Write-Host "Looking for Managed Identity with default name of the Logic App..."
-$miObjectID = (Get-AzureADServicePrincipal -SearchString "Import-AADSCAtoLAWS").ObjectId
-if ($miObjectID -eq $null) {
-   $miObjectID = Read-Host -Prompt "Enter ObjectId of Managed Identity (from Logic App):"
+Write-Host "Looking for Managed Identity with default prefix names of the Logic App..."
+$miObjectIDs = @()
+$miObjectIDs = (Get-AzureADServicePrincipal -SearchString "Import-AADSCA").ObjectId
+if ($miObjectIDs -eq $null) {
+   $miObjectIDs = Read-Host -Prompt "Enter ObjectId of Managed Identity (from Logic App):"
 }
 
 # The app ID of the Microsoft Graph API where we want to assign the permissions
 $appId = "00000003-0000-0000-c000-000000000000"
-
-
 $permissionsToAdd = @("Policy.Read.All", "ConsentRequest.Read.All", "Directory.Read.All","ServicePrincipalEndpoint.Read.All","Policy.Read.PermissionGrant","DirectoryRecommendations.Read.All")
-
 $app = Get-AzureADServicePrincipal -Filter "AppId eq '$appId'"
 
-foreach ($permission in $permissionsToAdd)
-{
-   Write-Host $permission
-   $role = $app.AppRoles | where Value -Like $permission | Select-Object -First 1
-   New-AzureADServiceAppRoleAssignment -Id $role.Id -ObjectId $miObjectID -PrincipalId $miObjectID -ResourceId $app.ObjectId
+foreach ($miObjectID in $miObjectIDs) {
+    foreach ($permission in $permissionsToAdd) {
+    Write-Host $permission
+    $role = $app.AppRoles | where Value -Like $permission | Select-Object -First 1
+    New-AzureADServiceAppRoleAssignment -Id $role.Id -ObjectId $miObjectID -PrincipalId $miObjectID -ResourceId $app.ObjectId
+    }
 }
